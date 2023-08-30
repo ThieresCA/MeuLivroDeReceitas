ff5b66bdf74d24245912b8207943384e55f8c9f5
ThieresCA <89056548+ThieresCA@users.noreply.github.com>
2023-08-29 21:47:14 -0300

    Adicionando o EF e criando a Database

--- src/Backend/MeuLivroDeReceitas.Api/Program.cs
+++ src/Backend/MeuLivroDeReceitas.Api/Program.cs
@@ -1,10 +1,13 @@
+using MeuLivroDeReceitas.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+ConfigureServices(builder.Services, builder.Configuration);
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -23,3 +26,11 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+static void ConfigureServices(IServiceCollection services, IConfiguration Configuration)
+{
+
+    services.AddDbContext<MeuLivroDeReceitasContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("MeuLivroDeReceitasDb"), builder =>
+                    builder.MigrationsAssembly("MeuLivroDeReceitas.Infrastructure")));
+}
