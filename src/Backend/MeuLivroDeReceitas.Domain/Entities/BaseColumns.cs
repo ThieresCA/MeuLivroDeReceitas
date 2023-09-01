f9e48cc27f198273ade46bc4b7b30938885d1fbf
ThieresCA <89056548+ThieresCA@users.noreply.github.com>
2023-09-01 19:15:26 -0300

    criacao de repositorio read e write only de Usuario e interface, criacao da ligacao da classe usuario a tabela usuario e os metodos de AddUser e EmailAlreadyExists

---
+++ src/Backend/MeuLivroDeReceitas.Domain/Entities/BaseColumns.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuLivroDeReceitas.Domain.Entities
+{
+    public class BaseColumns
+    {
+        public long Id { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
--- src/Backend/MeuLivroDeReceitas.Infrastructure/Data/MeuLivroDeReceitasContext.cs
+++ src/Backend/MeuLivroDeReceitas.Infrastructure/Data/MeuLivroDeReceitasContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MeuLivroDeReceitas.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace MeuLivroDeReceitas.Infrastructure.Data
@@ -10,6 +11,14 @@ namespace MeuLivroDeReceitas.Infrastructure.Data
     {
         public MeuLivroDeReceitasContext(DbContextOptions<MeuLivroDeReceitasContext> options) : base(options)
         {
+        }
+
+        // aqui é feito a conexão da tabela de usuário com a classe
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MeuLivroDeReceitasContext).Assembly);
         }
     }
 }
