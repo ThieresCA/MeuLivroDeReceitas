1901bdffc95de5164f9ef169fe4d5001de6e244c
ThieresCA <89056548+ThieresCA@users.noreply.github.com>
2023-10-04 17:08:31 -0300

    criado os testes para WebApi - Endpoint

---
+++ WebApi.Test/V1/User/ControllerBase.cs
@@ -0,0 +1,26 @@
+﻿using MeuLivroDeReceitas.Exceptions;
+using Newtonsoft.Json;
+using System.Globalization;
+using System.Text;
+using Xunit;
+
+namespace WebApi.Test.V1.User
+{
+    public class ControllerBase : IClassFixture<MeuLivroDeReceitasWebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _httpClient;
+
+        public ControllerBase(MeuLivroDeReceitasWebApplicationFactory<Program> factory)
+        {
+            _httpClient = factory.CreateClient();
+            ResourceErrorMessage.Culture = CultureInfo.CurrentCulture;
+        }
+
+        protected async Task<HttpResponseMessage> PostRequest(string metodo, object body)
+        {
+            var jsonString = JsonConvert.SerializeObject(body);
+
+            return await _httpClient.PostAsync(metodo, new StringContent(jsonString, Encoding.UTF8, "application/json"));
+        }
+    }
+}
