2354c4f7e25776c61c54bb21fe8ebbfa86a17041
Thieres Caetano <thieresca@gmail.com>
2024-02-22 15:45:19 -0300

    criado o test de validacao do UpdatePassword

---
+++ Validators.Test/UpdatePassword/UpdatePasswordTest.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+using MeuLivroDeReceitas.Application.UseCases.User.SignUp;
+using MeuLivroDeReceitas.Comunication.Request;
+using MeuLivroDeReceitas.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Validators.Test.UpdatePassword
+{
+    public class UpdatePasswordTest
+    {
+        [Fact]
+        public void Validate_Sucess()
+        {
+            var validator = new UpdatePasswordValidator();
+
+            var request = new UpdatePasswordRequestJson
+            {
+                Password = "string2",
+                NewPassword = "string3"
+            };
+
+            var response = validator.Validate(request.NewPassword);
+
+            response.IsValid.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Validate_Error_Min_Lenght()
+        {
+            var validator = new UpdatePasswordValidator();
+
+            var request = new UpdatePasswordRequestJson
+            {
+                Password = "string2",
+                NewPassword = "strin"
+            };
+
+            var response = validator.Validate(request.NewPassword);
+
+            response.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceErrorMessage.INVALID_PASSWORD_6_CHAR_MIN));
+        }
+
+        [Fact]
+        public void Validate_Error_Empty_Password()
+        {
+            var validator = new UpdatePasswordValidator();
+
+            var request = new UpdatePasswordRequestJson
+            {
+                Password = "string2",
+                NewPassword = ""
+            };
+
+            var response = validator.Validate(request.NewPassword);
+
+            response.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceErrorMessage.EMPTY_PASSWORD));
+        }
+    }
+}
